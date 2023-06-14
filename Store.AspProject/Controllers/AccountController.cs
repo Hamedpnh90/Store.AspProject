9ebab843993f03fe999eb1081317bf9c6effc30e
Hamedpnh90 <103711949+Hamedpnh90@users.noreply.github.com>
2023-06-14 11:16:25 +0330

    Add project files.

---
+++ Store.AspProject/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.AspProject.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        #region Register
+        [HttpGet("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        #endregion
+
+
+        #region Login
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
