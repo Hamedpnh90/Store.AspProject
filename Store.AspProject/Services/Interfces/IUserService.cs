6585c18ee4cf71773ae402a5e13c04b6a7b91174
Hamedpnh90 <103711949+Hamedpnh90@users.noreply.github.com>
2023-06-27 20:50:56 +0330

    AddUserServices

---
+++ Store.AspProject/Services/Interfces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Store.AspProject.DataLayer.Models.User;
+
+namespace Store.AspProject.Services.Interfces
+{
+    public interface IUserService
+    {
+
+        IList<User> GetAllUsers();
+
+        User GetUserById(int id);
+
+        bool EditeUser(User user);
+
+        bool DeleteUser(int id);    
+    }
+}
---
+++ Store.AspProject/Services/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Store.AspProject.DataLayer.Context;
+using Store.AspProject.DataLayer.Models.User;
+using Store.AspProject.Services.Interfces;
+
+namespace Store.AspProject.Services.Services
+{
+    public class UserService : IUserService
+    {
+
+        AspStoreDbContext _context;
+
+        public UserService(AspStoreDbContext context)
+        {
+            _context = context;
+        }
+        public bool DeleteUser(int id)
+        {
+            User user = _context.users.Find(id);    
+
+            if (user == null)   return false;   
+
+            _context.users.Remove(user);    
+            _context.SaveChanges(); 
+            return true;
+        }
+
+        public bool EditeUser(User user)
+        {
+            if(user == null) return false;
+
+            _context.users.Update(user);    
+            _context.SaveChanges(); 
+            return true;    
+        }
+
+        public IList<User> GetAllUsers()
+        {
+            return _context.users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _context.users.FirstOrDefault(u=>u.User_ID==id);
+        }
+    }
+}
