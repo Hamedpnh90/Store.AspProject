424e890ea95969b2714b3f60a94a852e8f3e0f78
Hamedpnh90 <103711949+Hamedpnh90@users.noreply.github.com>
2023-06-28 09:27:24 +0330

    DataTabel

--- Store.AspProject/Areas/Admin/Controllers/HomeController.cs
+++ Store.AspProject/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Store.AspProject.Services.Interfces;
 
 namespace Store.AspProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        IUserService _UserService;
+
+        public HomeController(IUserService UserService)
+        {
+            _UserService = UserService;
+        }
+      
         public IActionResult Index()
         {
-            return View();
+            var user = _UserService.GetAllUsers();
+            return View(user);
         }
     }
 }
--- Store.AspProject/Controllers/AccountController.cs
+++ Store.AspProject/Controllers/AccountController.cs
@@ -11,6 +11,8 @@ namespace Store.AspProject.Controllers
         {
             return View();
         }
+
+
         #endregion
 
 
